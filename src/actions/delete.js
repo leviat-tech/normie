f940034680e15f220ebe36fe0b356bddc6329a09
leearaneta <laraneta@crh.io>
2022-10-04 13:48:43 -0400

    fix: correctly apply foreign key opts when using belongsTo

--- src/actions/delete.js
+++ src/actions/delete.js
@@ -2,10 +2,10 @@ import { remove } from 'lodash-es'
 
 export default function (Entity, id) {
   Entity.dependentBelongsToRelations.forEach((belongsTo) => {
-    const { foreignKeyField, PrimaryEntity } = belongsTo
+    const { foreignKey, foreignKeyField, PrimaryEntity } = belongsTo
     // disassociate (or delete) all objects related to the deleted instance;
     PrimaryEntity.idsByForeignKey[foreignKeyField][id]?.forEach?.((_id) => {
-      if (belongsTo.foreignKey.onDeleteCascade) {
+      if (foreignKey.onDeleteCascade) {
         this.delete(PrimaryEntity, _id)
       } else {
         PrimaryEntity.dataById[_id][foreignKeyField] = null
--- test/foreign-key.spec.js
+++ test/foreign-key.spec.js
@@ -2,7 +2,7 @@ import { beforeEach, describe, it, expect } from 'vitest'
 import { defineStore, setActivePinia, createPinia } from 'pinia'
 import normie from '../src/normie'
 import Entity from '../src/entity'
-import { InvalidForeignKeyError, DoesNotExistError } from '../src/exceptions'
+import { DoesNotExistError } from '../src/exceptions'
 
 class Parent extends Entity {
   static id = 'parent'
@@ -13,8 +13,20 @@ class Parent extends Entity {
 class Child extends Entity {
   static id = 'child'
   static fields = {
-    parentId: this.foreignKey('parent', { onDeleteCascade: true }),
-    parent: this.belongsTo('parent', 'parentId')
+    parent: this.belongsTo(Parent, 'parentId', { onDeleteCascade: true })
+  }
+}
+
+class Layer extends Entity {
+  static id = 'layer'
+
+  static get fields () {
+    return {
+      ...this.baseFields,
+      name: 'Project Layer',
+      parent: this.belongsTo(Layer, 'parentId', { onDeleteCascade: true }),
+      layers: this.hasMany(Layer, 'parentId')
+    }
   }
 }
 
@@ -23,7 +35,7 @@ describe('foreign key validations', () => {
   let child
   beforeEach(() => {
     setActivePinia(createPinia())
-    normie(defineStore, [Parent, Child])
+    normie(defineStore, [Parent, Child, Layer])
     parent = Parent.create({ children: [{}] })
     child = Child.read()[0]
   })
@@ -31,6 +43,11 @@ describe('foreign key validations', () => {
   it('cascades deletion if a foreign key is onDeleteCascade', () => {
     parent.$delete()
     expect(Child.read().length).toBe(0)
+
+    const layer1 = Layer.create({})
+    const layer2 = Layer.create({ parentId: layer1.id })
+    layer1.$delete()
+    expect(Layer.find(layer2.id)).toBe(undefined)
   })
 
   it('cannot assign a nonexistent id to a foreign key on create', () => {
