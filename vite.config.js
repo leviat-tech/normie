9f591daa1fda3ac41e6649810f9d2afb29c3f988
kwasilonek <87523973+kwasilonek@users.noreply.github.com>
2025-02-12 11:56:05 +0100

    fix: convert entity to ts extension (#18)
    Co-authored-by: Kamil Wasilonek <kamilwasilonek@griddynamics.com>

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
 import path from 'path'
 import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.js'),
+      entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'normie',
       fileName: (format) => `normie.${format}.js`
     }
   },
-  plugins: [],
+  plugins: [
+    dts({
+      insertTypesEntry: true // Ensures entry point for types
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
